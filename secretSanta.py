2c3f3e4a877f5ee58a0961b88adb3fbb7c28bac5
Max Poschmann <44649744+maxposchmann@users.noreply.github.com>
2022-12-02 16:46:50 -0500

    Add files via upload

---
+++ secretSanta.py
@@ -0,0 +1,124 @@
+import csv
+import numpy as np
+import random
+import copy
+
+historyFile     = 'exampleData-history.csv'
+exclusionsFile  = 'exclusions.csv'
+currentYear     = 2022
+gifts           = 2
+samples         = 10000
+nonParticipants = []
+showScores      = False
+
+def readHistory(historyFile):
+    history = {}
+    with open(historyFile, newline='') as csvfile:
+        historyData = csv.reader(csvfile, delimiter=',', quotechar='"')
+        header = next(historyData)
+        years = [int(y) for y in header[1:]]
+        for row in historyData:
+            giver = row[0]
+            history[giver] = {}
+            for year, recipient in zip(years,row[1:]):
+                if recipient:
+                    if recipient in history[giver]:
+                        # List started, append to it
+                        history[giver][recipient].append(year)
+                    else:
+                        # Create list
+                        history[giver][recipient] = [year]
+    return history
+
+def readExclusions(exclusionsFile):
+    exclusions = {}
+    with open(exclusionsFile, newline='') as csvfile:
+        exclusionsData = csv.reader(csvfile, delimiter=',', quotechar='"')
+        header = next(exclusionsData)
+        for row in exclusionsData:
+            giver = row[0]
+            exclusions[giver] =list(filter(lambda a: a != '',row[1:]))
+    return exclusions
+
+history = readHistory(historyFile)
+exclusions = readExclusions(exclusionsFile)
+
+people = list(history.keys())
+# Exclude people not participating this year
+for nonParticipant in nonParticipants:
+    people.remove(nonParticipant)
+
+# Generate list of valid recipients for each giver
+recipients = {}
+for giver in people:
+    recipients[giver] = people.copy()
+    recipients[giver].remove(giver)
+    for exclusion in exclusions[giver]:
+        try:
+            recipients[giver].remove(exclusion)
+        except ValueError:
+            # Likely non-participant, possible duplicate
+            pass
+
+totalSamples = 0
+validSamples = 0
+best = np.inf
+bestCombo = {}
+for _ in range(samples):
+    totalSamples = totalSamples + 1
+    combo = {}
+    rating = 0
+    # Set up number of gifts required per person
+    remaining = {}
+    for person in people:
+        remaining[person] = gifts
+    # Loop over givers and choose recipients at random
+    tempRecipients = copy.deepcopy(recipients)
+    for giver in people:
+        combo[giver] = {}
+        try:
+            choice = random.sample(tempRecipients[giver],gifts)
+        except ValueError:
+            # If sample can't be drawn, cancel
+            rating = rating + np.inf
+            break
+
+        for person in choice:
+            individualRating = 0
+            # Calculate score
+            if person in history[giver].keys():
+                for year in history[giver][person]:
+                    individualRating = individualRating + 2.0**(year - currentYear)
+            # Store choice and score
+            combo[giver][person] = individualRating
+            rating = rating + individualRating
+            # Update potential recipient lists
+            # Remove from giver's list (no duplicate gifts)
+            tempRecipients[giver].remove(person)
+            # De-increment counters
+            remaining[person] = remaining[person] - 1
+            # If a recipient has enough givers, remove them from all lists
+            if remaining[person] == 0:
+                for potentialGiver in people:
+                    if person in tempRecipients[potentialGiver]:
+                        tempRecipients[potentialGiver].remove(person)
+    # Count valid samples
+    if rating < np.inf:
+        validSamples = validSamples + 1
+    # Compare rating to best
+    if rating < best:
+        best = rating
+        bestCombo = combo
+        if rating == 0:
+            # Call it for perfect score
+            break
+
+print(f'Best score was {best} after {totalSamples} total samples, {validSamples} valid samples')
+print()
+for giver in people:
+    print(f'Santa {giver} has recipients:')
+    if showScores:
+        print(', '.join([f'{k}: {v}' for k,v in bestCombo[giver].items()]))
+    else:
+        print(', '.join(bestCombo[giver].keys()))
+    print()
