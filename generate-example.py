f22438c603e2c684a7e9a11e6ed622c2557c42da
maxposchmann <maxposchmann@gmail.com>
2022-12-05 11:16:06 -0500

    Update generate example

--- generate-example.py
+++ generate-example.py
@@ -2,7 +2,9 @@ import secretSanta.santaReader as reader
 import secretSanta.generateData as generate
 
 exclusionsFile     = 'example-exclusions.csv'
-years              = [y for y in range(2015,2021)]
+startYear          = 1992
+endYear            = 2021
+years              = [y for y in range(startYear,endYear+1)]
 gifts              = 2
 samples            = 10000
 nonParticipants    = []
