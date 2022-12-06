fa4607f853b6bcd51caf9e182c903340eb34d4f3
maxposchmann <maxposchmann@gmail.com>
2022-12-06 14:31:13 -0500

    Sort people list based on number of exclusions

--- secretSanta/santaSetup.py
+++ secretSanta/santaSetup.py
@@ -6,4 +6,14 @@ def setup(historyFile,exclusionsFile):
 
     people = list(history.keys()) + list(set(exclusions.keys()) - set(history.keys()))
 
+    numberOfExclusions = []
+    for person in people:
+        try:
+            numberOfExclusions.append(len(exclusions[person]))
+        except KeyError:
+            numberOfExclusions.append(0)
+
+    # Sort list by number of exclusions for better performance
+    people = [x for _,x in sorted(zip(numberOfExclusions, people),reverse=True)]
+    
     return people, history, exclusions
